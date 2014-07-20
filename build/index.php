53cae3e61610ddda64e222f5233a5ee6cdcd8484
Brandon Okert <bcokert@gmail.com>
2014-07-20 13:02:33 -0700

    Added a basic index to the dispatcher that explains what it is

---
+++ build/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Psched Homepage</title>
+    <link rel="stylesheet" type="text/css" href="static/css/style.css">
+</head>
+<body>
+<p>This is psched-dispatcher-1, working for psched-main-1</p>
+</body>
+</html>
